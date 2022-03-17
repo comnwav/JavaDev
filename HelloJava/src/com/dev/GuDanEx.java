21836fd813d53121d0a21ee79342ecf18ccedc3f
tizio <tizio@comnwav>
2022-03-18 01:18:04 +0900

    구구단 완성

--- HelloJava/src/com/dev/GuDanEx.java
+++ HelloJava/src/com/dev/GuDanEx.java
@@ -3,8 +3,18 @@ package com.dev;
 public class GuDanEx {
 
 	public static void main(String[] args) {
-		
-		
+
+		String danSum = "";
+		int cnt = 1;
+
+		for (int j = 0; j < 9; j++) {
+			for (int i = 0; i < 8; i++) {
+				danSum += (i + 2) + " * " + cnt + " = " + ((i + 2) * cnt) + "  ";
+			}
+			System.out.println(danSum);
+			danSum = "";
+			cnt++;
+		}
 
 	}
 
