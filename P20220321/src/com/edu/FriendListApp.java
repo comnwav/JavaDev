5d783862ef2327f516dfc8f3bb1097f6c9148b20
admin <admin@YD-20>
2022-03-21 12:36:03 +0900

    utf-8

--- P20220321/src/com/edu/FriendListApp.java
+++ P20220321/src/com/edu/FriendListApp.java
@@ -3,32 +3,41 @@ package com.edu;
 public class FriendListApp {
 
 	public static void main(String[] args) {
-		
+
 		Friend[] friends = new Friend[10];
-		
+
 		Friend f1 = new Friend();
 		f1.setName("서현일");
 		f1.setAge(25);
 		f1.setHeight(1792);
 		f1.setWeight(72.5);
 		f1.setPhone("01099827345");
-		
+
 		UniFriend f2 = new UniFriend("서현일", 29, 178.3, 69.2, "010-9982-7345");
 		f2.setUniversity("영남대학교");
 		f2.setMajor("컴퓨터공학과");
-		
+
 		ComFriend f3 = new ComFriend("최규완", 29, 179.3, 75.2, "010-1341-7349", "네이버", "지도app개발연구원");
-		
+
 		friends[0] = f1;
 		friends[1] = f2;
 		friends[2] = f3;
-		
-		for (int i=0; i < friends.length; i++) {
-			if(friends[i] != null)
-			System.out.println(friends[i].toString());
-		}
-		
 
+		for (int i = 0; i < friends.length; i++) {
+			if (friends[i] != null)
+				System.out.println(friends[i].toString());
+		}
+
+		// UniFriend; getUniversity, getMajor
+		Friend friend = (Friend) f2; 
+		friend = f1;
+		UniFriend uni = null;
+		// instanceof 해당 클래스의 인스턴스인지 체크 그리고 casting 해야함
+		if (uni instanceof UniFriend) {
+			uni = (UniFriend) friend;
+			// 부모클래스의 참조변수 > 자식 클래스의 참조변수 할당가능
+			uni.getUniversity();
+		}
 	}
 
 }
