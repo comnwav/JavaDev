acd7032b25927622030a53f4d4fe8f1c45831851
admin <admin@YD-20>
2022-03-31 17:20:04 +0900

    hello

--- P20220321/src/com/edu/collect/StudentService.java
+++ P20220321/src/com/edu/collect/StudentService.java
@@ -8,13 +8,16 @@ public interface StudentService {
 
 	public void insertStudent(Student student); // 입력
 
+	public void modifyStudent(Student student); // 한건수정
+	public void removeStudent(int sno); // 한건삭제
+	
+	
+	
 	public Student getStudent(int sno); // 한건조회
 
 	public List<Student> studentList(); // 전체목록
 
-	public void modifyStudent(Student student); // 한건수정
 
-	public void removeStudent(int sno); // 한건삭제
 
 	public List<Student> searchStudent(String name); // 이름조회, 여러건이 조회될 가능성이 있음
 
